d0bd6d834799bcee5de1f1fe44987d69dcab4fb9
Matheus P <matheuspn12@gmail.com>
2020-01-16 19:29:31 -0300

    Some tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope :orders do
-    post 'create', to: 'orders#create'
-    get 'status/reference/:reference', to: 'orders#reference_status'
-    get 'status/client/:client_name', to: 'orders#client_status'
-    get 'list/:purchase_channel', to: 'orders#list'
+    post 'create',
+         to: 'orders#create', as: 'create_order'
+    get 'status/reference/:reference',
+        to: 'orders#reference_status', as: 'order_reference_status'
+    get 'status/client/:client_name',
+        to: 'orders#client_status', as: 'order_client_status'
+    get 'list/:purchase_channel',
+        to: 'orders#list', as: 'order_list'
   end
 
   scope :batches do
-    post 'create/:purchase_channel', to: 'batches#create'
-    post 'produce/:reference', to: 'batches#produce'
-    post 'close/:reference/:delivery_service', to: 'batches#close'
+    post 'create/:purchase_channel',
+         to: 'batches#create', as: 'batch_create'
+    post 'produce/:reference',
+         to: 'batches#produce', as: 'batch_produce'
+    post 'close/:reference/:delivery_service',
+         to: 'batches#close', as: 'batch_close'
   end
 end
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,69 @@
 require 'test_helper'
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should return ok for status by reference' do
+    get order_reference_status_path('UNIQUEREF1')
+    assert_response :ok
+  end
+
+  test 'should return no content for status by non-existent reference' do
+    get order_reference_status_path('DOESNOTEXIST')
+    assert_response :no_content
+  end
+
+  test 'should return ok for order status by client_name' do
+    get order_client_status_path('NAME1')
+    assert_response :ok
+  end
+
+  test 'should return no content for status by non-existent client_name' do
+    get order_client_status_path('DOESNOTEXIST')
+    assert_response :no_content
+  end
+
+  test 'list should always return ok' do
+    %w[CHAN NONEXIST].each do |purchase_channel|
+      get order_list_path(purchase_channel)
+      assert_response :ok
+    end
+  end
+
+  test 'should create order and return created' do
+    assert_difference 'Order.count', 1 do
+      post create_order_path, params: {
+        order: {
+          reference: 'UNIQUEREF2',
+          purchase_channel: 'CHAN2',
+          client_name: 'NAME2',
+          address: 'ADDR',
+          delivery_service: 'DS',
+          total_value: 'CURR',
+          line_items: [
+            '{ sku: case-my-best-friend, model: iPhone X, case type: Rose Leather}',
+            '{ sku: powebank-sunshine, capacity: 10000mah}',
+            '{sku: earphone-standard, color: white}'
+            ]
+        }
+      }
+      assert_response :created
+    end
+  end
+
+  test 'should not create order and should return unprocessable entity' do
+    assert_difference 'Order.count', 0 do
+      post create_order_path, params: {
+        order: {
+          reference: '',
+          purchase_channel: '',
+          client_name: '',
+          address: '',
+          delivery_service: '',
+          total_value: '',
+          line_items: []
+        }
+      }
+      assert_response :unprocessable_entity
+    end
+  end
+
 end
